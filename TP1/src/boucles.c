e68a534eb4816e2c9863990f911579871276d4c5
SPIECARTLUCAS <lucas.spiecart@gmail.com>
2025-04-16 16:39:10 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
-int main() 
-{
+int main() {
     int compteur = 5; // Vous pouvez changer cette valeur pour tester différentes tailles
 
     for (int i = 1; i <= compteur; i++) 
     {
         for (int j = 1; j <= i; j++) 
         {
-            if (j % 2 == 0) 
+            if (j == 1 || j == i || i == compteur) 
             {
-                printf("# ");
+                printf("* ");
             } 
             else 
             {
-                printf("* ");
+                printf("# ");
             }
         }
         printf("\n");
