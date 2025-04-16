d483db3078a96f16d8994ece08d20008bd675891
SPIECARTLUCAS <lucas.spiecart@gmail.com>
2025-04-16 15:08:30 +0200

    Update sizeof_types.c

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Taille de 'char' : %zu octets\n", sizeof(char));
+    printf("Taille de 'signed char' : %zu octets\n", sizeof(signed char));
+    printf("Taille de 'unsigned char' : %zu octets\n", sizeof(unsigned char));
+
+}
