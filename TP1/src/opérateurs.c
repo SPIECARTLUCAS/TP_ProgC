e860e56fabee44fe8af2f4ff8d24b27853f0a7fd
SPIECARTLUCAS <lucas.spiecart@gmail.com>
2025-04-16 15:50:43 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -6,30 +6,25 @@ int main() {
     int b = 3;
 
     int addition = a + b;
-    printf("a + b %d\n", addition);
-
+    printf("a + b = %d\n", addition);
 
     int soustraction = a - b;
-    printf("a - b : %d\n", soustraction);
-
+    printf("a - b = %d\n", soustraction);
 
     int multiplication = a * b;
-    printf("a*b : %d\n", multiplication);
-
+    printf("a * b = %d\n", multiplication);
 
     int division = a / b;
-    printf("a/b: %d\n", division);
-
+    printf("a / b = %d\n", division);
 
     int modulo = a % b;
-    printf("a%b : %d\n", modulo);
+    printf("a %% b = %d\n", modulo); // Utilisez %% pour afficher le caractère %
 
- 
     bool est_egal = (a == b);
-    printf("a est égal à b : %s\n", est_egal);
-  
+    printf("a est égal à b : %s\n", est_egal ? "vrai" : "faux");
+
     bool est_superieur = (a > b);
-    printf("a est supérieur à b : %s\n", est_superieur);
+    printf("a est supérieur à b : %s\n", est_superieur ? "vrai" : "faux");
 
     return 0;
 }
