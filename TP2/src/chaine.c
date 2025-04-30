42b136a41a31f66d68184edd7b14d36a58192784
SPIECARTLUCAS <lucas.spiecart@gmail.com>
2025-04-30 15:36:17 +0200

    Update chaine.c

---
+++ TP2/src/chaine.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+
+int calculerLongueur(char *chaine) {
+    int longueur = 0;
+    while (chaine[longueur] != '\0') {
+        longueur++;
+    }
+    return longueur;
+}
+
+
+void copierChaine(char *source, char *destination) {
+    int i = 0;
+    while (source[i] != '\0') {
+        destination[i] = source[i];
+        i++;
+    }
+    destination[i] = '\0'; 
+}
+
+
+void concatenerChaine(char *chaine1, char *chaine2, char *resultat) {
+    int i = 0, j = 0;
+
+    
+    while (chaine1[i] != '\0') {
+        resultat[j++] = chaine1[i++];
+    }
+
+    i = 0;
+    while (chaine2[i] != '\0') {
+        resultat[j++] = chaine2[i++];
+    }
+
+    resultat[j] = '\0'; 
+}
+
+int main() {
+    char chaine1[] = "Hello";
+    char chaine2[] = " World!";
+    char copie[50];
+    char resultat[100];
+
+    int longueur1 = calculerLongueur(chaine1);
+    printf("Longueur de '%s' : %d\n", chaine1, longueur1);
+
+    copierChaine(chaine1, copie);
+    printf("Copie de '%s' : %s\n", chaine1, copie);
+
+    concatenerChaine(chaine1, chaine2, resultat);
+    printf("Concaténation de '%s' et '%s' : %s\n", chaine1, chaine2, resultat);
+
+}
