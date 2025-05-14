7a5f9c85c7b5d1ad434bdbf2c19b846b2df629b5
SPIECARTLUCAS <lucas.spiecart@gmail.com>
2025-05-14 17:31:53 +0200

    Update serveur.c

--- TP5/src/serveur.c
+++ TP5/src/serveur.c
@@ -2,56 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define PORT 12345
 
 int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char message[1024];
 
     // Créer le socket
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket creation error");
+        return -1;
     }
 
-    // Configurer l'adresse
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    // Configurer l'adresse du serveur
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "10.0.39.7", &serv_addr.sin_addr); // IP du serveur
 
-    // Lier le socket
-    if (bind(server_fd, (struct sockaddr )&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    // Connexion
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connexion échouée");
+        return -1;
     }
 
-    // Écouter
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+    printf("Connecté au serveur !\n");
 
-    printf("Serveur en attente de connexion...\n");
-
-    // Accepter une connexion
-    new_socket = accept(server_fd, (struct sockaddr)&address, (socklen_t*)&addrlen);
-    if (new_socket < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Client connecté !\n");
-
-    // Lire les messages
+    // Boucle d'envoi de messages
     while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        read(new_socket, buffer, 1024);
-        printf("Reçu : %s\n", buffer);
+        fgets(message, sizeof(message), stdin);
+        send(sock, message, strlen(message), 0);
     }
 
     return 0;
